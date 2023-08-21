79b1ce65c4a0e8c602e7e75b15d72c9e624d0297
karinanserrano <karinaserrano90@gmail.com>
2023-08-21 19:37:30 -0300

    agregando task 1

---
+++ scriptPrueba.js
@@ -0,0 +1,14 @@
+/* 
+let nombrePorConsola=prompt('Ingrese un nombre: ')
+console.log("Su nombre ingresado es "+ nombrePorConsola)/* console.log("Hola mundo")
+let nombre="Karina"
+console.log(nombre)
+let persona={
+    nombre:"Dany",
+    edad:44,
+}
+console.log(persona) 
+console.log(persona.edad)
+let frutas=["pera","sandia","uva"]
+console.log(frutas)
+console.log(frutas[0 ,1]) */
